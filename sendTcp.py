0e74e446349a6704b566c4472df0d653ade3abd3
despaink <43147974+despaink@users.noreply.github.com>
2020-04-17 18:30:49 -0700

    demo changes.

--- sendTcp.py
+++ sendTcp.py
@@ -17,17 +17,17 @@ selectBad = '** select 123, hostname, 0'
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 28801)
+server_address = ('kPi', 28801)
 print ('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
 try:    
     # Send data
-    message = tooMany
+    message = select
     print ('sending "%s"' % message)
     sock.sendall(message.encode())
     
-    data = recvall(sock).decode()
+    data = recvall(sock)
     print('recieved: {}'.format(data))
 
 finally:
